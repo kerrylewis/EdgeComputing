3ae1c5fccee04d38a1d208c770402955cfaf2b4e
klewis4 <kerry.lewis0097@gmail.com>
2018-04-11 22:01:59 +0100

    Added self to producer file

--- KafkaProducer.py
+++ KafkaProducer.py
@@ -5,7 +5,7 @@ import time
 
 class ProduceMessage(object):
 
-    def producer(data):
+    def producer(self, data):
         # initialise the producer so that the broker can respond to the API request
         producer = KafkaProducer(bootstrap_servers='192.168.1.130', value_serializer=lambda m: json.dumps(m).encode('ascii'))
         # produce readings every 3 seconds
