0a7a8d7a3ac93fea79de2f050596905a70006bc4
kerrylewis <kerry.lewis0097@gmail.com>
2018-04-17 10:52:43 +0100

    Update KafkaProducer.py

--- KafkaProducer.py
+++ KafkaProducer.py
@@ -7,7 +7,7 @@ class ProduceMessage(object):
 
     def producer(self, data):
         # initialise the producer so that the broker can respond to the API request
-        producer = KafkaProducer(bootstrap_servers='192.168.1.130', value_serializer=lambda m: json.dumps(m).encode('ascii'))
+        producer = KafkaProducer(bootstrap_servers='192.168.1.131:9092,192.168.1.132:9092,192.168.1.133:9092', value_serializer=lambda m: json.dumps(m).encode('ascii'))
         # produce readings every 3 seconds
         for reading in data:
             producer.send('aq-data', reading)
