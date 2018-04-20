741678adea768a3d7e63eec524157bb3505dcbea
klewis4 <kerry.lewis0097@gmail.com>
2018-04-20 09:36:48 +0100

    Removed redundantcode in run.py

--- run.py
+++ run.py
@@ -5,12 +5,6 @@ import sys
 from datetime import datetime
 
 if __name__ == '__main__':
-
-    now = str(datetime.now())
-    print(type(now))
-    print("now as string: " + now)
-    nowDate = datetime.strptime(now, "%Y-%m-%d %H:%M:%S.%f")
-    print(type(nowDate))
     if str(sys.argv[1]) == "producer":
         parsedReadings = ParseJson().parser()
         ProduceMessage().producer(parsedReadings)
