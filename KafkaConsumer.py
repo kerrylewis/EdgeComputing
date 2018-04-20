cd03970656ea35af46570ee0899ba3d867a51ddc
klewis4 <kerry.lewis0097@gmail.com>
2018-04-20 10:24:13 +0100

    Altered consumer to calculate latency and pass send rate into producer

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -12,10 +12,23 @@ class ConsumeMessage(object):
         # print the consumed messages and return array of data
         consumedData = []
         for msg in consumer:
-           print(json.loads(msg.value.decode("utf-8")))
-           sendTime = datetime.strptime(msg[0], "%Y-%m-%d %H:%M:%S.%f")
            receiveTime = datetime.now()
+           record = json.loads(msg.value.decode("utf-8"))
+           #print(str(msg[6]["timestamp"]))
+           sendTime = datetime.strptime(record["timestamp"], "%Y-%m-%d %H:%M:%S.%f")
+
            latency = receiveTime - sendTime
-           print("Latency:" + latency)
+           consumedData.append(latency);
+
+           if(len(consumedData) == 100):
+               avgLatency = None
+               for latency in consumedData:
+                   if avgLatency is None:
+                       avgLatency = latency
+                   else:
+                       avgLatency = avgLatency + latency
+               result = avgLatency / 100
+               print(result)
+           #print("Latency:" + str(latency))
         return consumedData
         # print consumer metrics
--- KafkaProducer.py
+++ KafkaProducer.py
@@ -6,13 +6,13 @@ from datetime import datetime
 
 class ProduceMessage(object):
 
-    def producer(self, data):
+    def producer(self, data, sendRate):
         # initialise the producer so that the broker can respond to the API request
         producer = KafkaProducer(bootstrap_servers='192.168.1.131:9092,192.168.1.132:9092,192.168.1.133:9092', value_serializer=lambda m: json.dumps(m).encode('ascii'))
         # produce readings every 3 seconds
         for reading in data:
             reading["timestamp"] = str(datetime.now())
             producer.send('aq-data', reading)
-            time.sleep(3)
+            time.sleep(sendRate)
             # prints the producer performance metrics
-        producer.close(timeout=60)
+        producer.close(timeout=sendRate*120)
